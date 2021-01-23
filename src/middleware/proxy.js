f4f0e90f2413f2ab4f54f7f5766074ad43fb2ca3
kaygod <184641801@qq.com>
2021-01-23 21:49:56 +0800

    add comment

--- src/middleware/proxy.js
+++ src/middleware/proxy.js
@@ -2,7 +2,7 @@ import proxy from 'koa-server-http-proxy';
 
 export const proxyHanlder = (app)=>{
     app.use(proxy('/api', {
-        target: 'https://geoapi.qweather.com',
+        target: 'https://geoapi.qweather.com', //网上寻找的开放API接口,支持返回地理数据.如果发现本接口失效了,可以将本页面target和vuex/store.js的getList方法换上新接口地址
         pathRewrite: { '^/api': '' },
         changeOrigin: true
     }));
